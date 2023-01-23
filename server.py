fd8af2a5ae55f691913730c94df806e12379d60f
lxndrbnsv <abnosov@gmail.com>
2023-01-23 12:01:06 +0100

    initial

---
+++ server.py
@@ -0,0 +1,34 @@
+import asyncio
+
+import websockets
+
+
+async def handler1(websocket):
+    while True:
+        message = await websocket.recv()
+        print("handler1 :", message)
+
+
+async def handler2(websocket):
+    while True:
+        message = await websocket.recv()
+        print("handler2 :", message)
+
+
+async def sock1():
+    async with websockets.serve(handler1, "", 8001):
+        await asyncio.Future()
+
+
+async def sock2():
+    async with websockets.serve(handler2, "", 8002):
+        await asyncio.Future()
+
+
+async def main():
+    await asyncio.gather(sock1(), sock2())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
